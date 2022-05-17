0ecfc7b4a9c071eb6068f33bb9348cb3c00b5bbf
w3arthur <78190695+w3arthur@users.noreply.github.com>
2022-05-17 20:41:00 +0300

    hgjghjgh

--- App.js
+++ App.js
@@ -2,55 +2,43 @@ import React, { useState, useRef,  useEffect } from 'react';
 import { Platform, StatusBar as StatusBarAndroid, SafeAreaView, StyleSheet, ViewPagerAndroidBase } from 'react-native';
 import { Text as NativeText, View as NativeView,  KeyboardAvoidingView, Button as NativeButton} from 'react-native';
 import { AntDesign, Ionicons, Zocial, FontAwesome, MaterialIcons, Entypo } from '@expo/vector-icons';
+import { ScrollView, Heading, Text, Flex,Center, Box, Spacer , Button, Icon, Image, NativeBaseProvider, Container,} from "native-base";
 
 import { NavigationContainer }  from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { useNavigation } from '@react-navigation/native';
-import { MainPageContainer } from './Components'
-import {GithubFinder ,ArrayApp} from './Content'
+import { MainPageContainer, PageContainer } from './Components'
+import {GithubFinder, GithubUserData ,ArrayApp} from './Content'
+import {useGoBack, useGoTo} from './Hooks'
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [userMoreData, setUserMoreData] = useState();
 return (<MainPageContainer>
 
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Index Screen'  component={IndexScreen}/>
-        
-        <Stack.Screen name='Github Finder'  component={GithubFinder}/>
- 
-        <Stack.Screen name='Array App'  component={ArrayApp}/>
+        <Stack.Screen name='IndexScreen' options={{ title: 'Arthur Zarankin, React Native' }}  component={IndexScreen}/>
+        <Stack.Screen name='GithubFinder' options={{ title: 'Github Finder' }} component={() => GithubFinder ({userMoreData, setUserMoreData})}/>
+        <Stack.Screen name='GithubUserMoreData' options={{ title: 'Github User More Data' }} component={() => GithubUserData({userMoreData})}/>
+        <Stack.Screen name='ArrayApp' options={{ title: 'Array App' }} component={ArrayApp}/>
       </Stack.Navigator>
     </NavigationContainer>
 </MainPageContainer>);
 }
 
-
-
-
-function IndexScreen({navigation}) {
-  return (<>
-  
-  <NativeView>
-      <NativeText>Index</NativeText>
-      <NativeButton title="Github Finder App" onPress={()=>{navigation.navigate('Github Finder');}}></NativeButton>
-      <NativeButton title="Array App" onPress={()=>{navigation.navigate('Array App');}}></NativeButton>
-  </NativeView>
-
-
-
-  </>);
-}
-
-function Screen2({navigation}) {
-  const nav = useNavigation();
-  return (<NativeView>
-      <NativeText>Screen 2</NativeText>
-      <NativeButton title="Goto Page3" onPress={()=>{nav.navigate('page3');}}></NativeButton>
-      <NativeButton title="Back" onPress={()=>{nav.goBack();}}></NativeButton>
-
-  </NativeView>);
+function IndexScreen() {
+  const goTo = useGoTo();
+  return (<PageContainer>
+  <Box style={{flex: 1}}>
+    <Box style={{flex: 1}} pt={2}>
+      <Button m={2} leftIcon={<Icon as={MaterialIcons} name="person-search" size="lg" />} onPress={()=>{goTo('GithubFinder');}}>Github Finder App</Button>
+      <Button m={2} leftIcon={<Icon as={AntDesign} name="bars" size="lg" />} onPress={()=>{goTo('ArrayApp');}}>Array App</Button>
+    </Box>
+  </Box>
+  </PageContainer>);
 }
 
 
