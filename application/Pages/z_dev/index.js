78701fb549093070b10d4fde4357188c4c95a8a6
w3arthur <78190695+w3arthur@users.noreply.github.com>
2022-05-26 14:28:03 +0300

    delete practice files

--- application/Pages/z_dev/index.js
+++
@@ -1,7 +0,0 @@
-import ArrayApp from './ArrayApp';
-import Flights from './Flights';
-import GithubFinder from './GithubFinder';
-import GithubUserData from './GithubUserData';
-import ManagePersons from './ManagePersons';
-
-export { ArrayApp, Flights, GithubFinder, GithubUserData, ManagePersons };
