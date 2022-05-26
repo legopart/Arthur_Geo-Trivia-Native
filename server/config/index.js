c9f1228a28088c7248a5a7c64f3c4a85fa4c1f8b
w3arthur <78190695+w3arthur@users.noreply.github.com>
2022-05-26 14:14:39 +0300

    folder fix

---
+++ server/config/index.js
@@ -0,0 +1,13 @@
+const allowedAccess = require('./allowedAccess');
+const auth = require('./auth');
+const database = require('./database');
+const ports = require('./ports');
+const tokens = require('./tokens');
+
+module.exports = {
+    allowedAccess
+    , auth
+    , database
+    , ports
+    , tokens
+};
---
+++ server/routers/index.js
@@ -0,0 +1,8 @@
+const loginRouter = require('./login.router');
+
+const movieRouter = require('./movie.router');
+
+module.exports = {
+    movieRouter
+    , loginRouter
+};
